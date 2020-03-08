f703668a4aa9ded05f8519445bab475750c8a463
Joel Shuart <joel.shuart@gmail.com>
2020-03-08 12:33:32 -0400

    updated git ignore, inital project push, basic test space, models off asset store, basic camer amanager and singleton system

---
+++ ProjectKayak/Assets/Scripts/Managers/CameraManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    private Camera[] cameras;
+    private Camera activeCamera;
+    private int activeIndex;
+    [SerializeField] private Camera defaultCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameras = FindObjectsOfType<Camera>();
+
+        //turn off non active
+        foreach(Camera cam in cameras)
+        {
+            cam.enabled = false;
+        }
+
+        //turn on default camera- if none avaiable use first in list
+        if(defaultCamera == null) { defaultCamera = cameras[0]; }
+        activeCamera = defaultCamera;
+        activeCamera.enabled = true;
+        activeIndex = System.Array.IndexOf(cameras, activeCamera);
+    }
+
+    private void CycleCamera()
+    {
+        //disable current camera
+        activeCamera.enabled = false;
+
+        //up active cam
+        activeIndex = (activeIndex + 1) % (cameras.Length);
+        activeCamera = cameras[activeIndex];
+        activeCamera.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            CycleCamera();
+        }
+    }
+}
