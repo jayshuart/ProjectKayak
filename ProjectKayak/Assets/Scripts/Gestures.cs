d484b67a492dfc218e585476fddb55be5806d137
Joel Shuart <joel.shuart@gmail.com>
2020-03-10 00:00:56 -0400

    add swiping (gesture) based movement system

---
+++ ProjectKayak/Assets/Scripts/Gestures.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Gestures : MonoBehaviour
+{
+    public enum GESTURE_TYPE
+    {
+        UNSET,
+        SWIPE_DOWN,
+        SWIPE_UP,
+        SWIPE_LEFT,
+        SWIPE_RIGHT,
+        TAP,
+        TAP_DOUBLE
+    }
+    private Vector2 inputStart;
+    private Vector2 inputEnd;
+    private bool gestureStarted;
+    private const float MIN_SWIPE_LENGTH = 150;
+
+    //screen zone properties
+    private float SCREEN_CENTER_X = Screen.width / 2;
+    private float SCREEN_CENTER_Y = Screen.height / 2;
+    private float SCREEN_LEFT_SIDE = Screen.width / 4;
+    private float SCREEN_RIGHT_SIDE = Screen.width - (Screen.width / 4);
+
+    //swipe gesture delgates
+    public UnityEvent event_SwipeLeft;
+    public UnityEvent event_SwipeRight;
+    public UnityEvent event_SwipeUp;
+    public UnityEvent event_SwipeDown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("[Gestures] -- Initalized");
+        inputStart = Vector2.zero;
+        inputEnd = Vector2.zero;
+        gestureStarted = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if((Input.touchCount > 0 || Input.GetMouseButtonDown(0)) && !gestureStarted)
+        {
+            StartGesture();
+        }
+        else if((Input.touchCount <= 0 && !Input.GetMouseButton(0)) && gestureStarted)
+        {
+            EndGesture();
+        }
+        else if((Input.touchCount > 0 || Input.GetMouseButton(0)) && gestureStarted)
+        {
+            UpdateGesture();
+        }
+    }
+
+    private void StartGesture()
+    {
+        gestureStarted = true;
+
+        //get inital gesture position
+    #if UNITY_EDITOR
+        inputStart = Input.mousePosition;
+    #else
+        inputStart = Input.GetTouch(0).position;
+    #endif
+
+        Debug.Log("[Gestures] -- Started: " + inputStart);
+    }
+
+    private void UpdateGesture()
+    {
+        #if UNITY_EDITOR
+            inputEnd = Input.mousePosition;
+        #else
+            inputEnd = Input.GetTouch(0).position;
+        #endif
+    }
+
+    private void EndGesture()
+    {
+        gestureStarted = false;
+
+        //get vector between start and here
+        Vector2 diff = inputEnd - inputStart;
+
+        Debug.Log("[Gestures] -- Ended: " + inputEnd);
+        Debug.Log(diff.magnitude);
+
+        if(diff.magnitude >= MIN_SWIPE_LENGTH)
+        { CalculateGesture(diff); }
+    }
+
+    private void CalculateGesture(Vector2 gestureLine)
+    {
+        //wittle down possible gestures based on start pos
+        if(inputStart.x <= SCREEN_LEFT_SIDE) //left side start
+        {
+            if(inputEnd.x >= SCREEN_RIGHT_SIDE)
+            {
+                event_SwipeRight.Invoke();
+            }
+
+        }
+        else if(inputStart.x >= SCREEN_RIGHT_SIDE) //right side start
+        {
+            if(inputEnd.x <= SCREEN_LEFT_SIDE)
+            {
+                event_SwipeLeft.Invoke();
+            }
+        }
+        else //middle start
+        {
+            if(inputStart.y <= SCREEN_CENTER_Y && inputEnd.y >= SCREEN_CENTER_Y)
+            {
+                event_SwipeUp.Invoke();
+            }
+            else if(inputStart.y >= SCREEN_CENTER_Y && inputEnd.y <= SCREEN_CENTER_Y)
+            {
+                event_SwipeDown.Invoke();
+            }
+        }
+    }
+}
--- ProjectKayak/Assets/Scripts/Player.cs
+++ ProjectKayak/Assets/Scripts/Player.cs
@@ -9,55 +9,88 @@ public class Player : MonoBehaviour
     [SerializeField] private float PADDLE_TURN_POWER = 5;
 
     private Animator animator;
+    private Gestures gestureHandler;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = this.gameObject.GetComponent<Rigidbody>();
         animator =  this.gameObject.GetComponent<Animator>();
+        gestureHandler = this.gameObject.GetComponent<Gestures>();
 
         //animator.Play("Paddle_idle");
         animator.Play("float");
+
+        //setup gesture handler functions
+        gestureHandler.event_SwipeDown.AddListener(PaddleForward);
+        gestureHandler.event_SwipeUp.AddListener(PaddleBackward);
+        gestureHandler.event_SwipeLeft.AddListener(PaddleRight);
+        gestureHandler.event_SwipeRight.AddListener(PaddleLeft);
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if(Input.GetKeyDown(KeyCode.UpArrow))
         {
-            PaddleForward(1);
-
-            animator.Play("row_right");
-            animator.Play("row_left");
+            PaddleForward();
         }
         else if(Input.GetKeyDown(KeyCode.DownArrow))
         {
-            PaddleForward(-1);
-
-            animator.Play("row_right");
-            animator.Play("row_left");
+            PaddleBackward();
         }
 
         if(Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            PaddleTurn(-1);
-            animator.Play("row_left");
+            PaddleLeft();
         }
         else if(Input.GetKeyDown(KeyCode.RightArrow))
         {
-            PaddleTurn(1);
-            animator.Play("row_right");
+           PaddleRight();
         }
     }
 
-    private void PaddleForward(int pDir)
+    private void PaddleForward() 
+    {
+        StartCoroutine(DelayedMoveForward(.3f));
+        animator.Play("row_right");
+        animator.Play("row_left");
+    }
+
+    private void PaddleBackward()
+    {
+        MoveForward(-1);
+        animator.Play("row_right");
+        animator.Play("row_left");
+    }
+
+    private void PaddleLeft()
+    {
+        Turn(-1);
+        animator.Play("row_left");
+    }
+
+    private void PaddleRight()
+    {
+        Turn(1);
+        animator.Play("row_right");
+    }
+
+    IEnumerator DelayedMoveForward(float time)
+    {
+        yield return new WaitForSeconds(time);
+        MoveForward(1);
+    }
+
+    private void MoveForward(int pDir)
     {
         Vector3 paddleForce = this.transform.forward * PADDLE_POWER * pDir;
         rb.AddForce(paddleForce, ForceMode.Impulse);
         //rb.velocity += paddleForce;
     }
 
-    private void PaddleTurn(int pDir)
+    private void Turn(int pDir)
     {
         Vector3 paddleForce = new Vector3(0, PADDLE_TURN_POWER * pDir, 0);
         rb.AddTorque(paddleForce);
